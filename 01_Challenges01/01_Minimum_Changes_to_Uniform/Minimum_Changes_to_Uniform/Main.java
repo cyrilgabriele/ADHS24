e8925b0a5ae6d5475d6a35f7eb4e809a5db93f45
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-10-15 21:39:55 +0200

    first working version. However a bug in the merge sort while implementing like in lecture... used a different implementation but same concept (from G4G)

--- 01_Challenges01/01_Minimum_Changes_to_Uniform/Minimum_Changes_to_Uniform/Main.java
+++ 01_Challenges01/01_Minimum_Changes_to_Uniform/Minimum_Changes_to_Uniform/Main.java
@@ -8,10 +8,10 @@ class Main {
 
     int testNum = In.readInt(); // number of tests
     for (int test = 0; test < testNum; test++) {
-      int n = In.readInt(); // size of array
+      int n = In.readInt(); // size of Aay
       int[] A = new int[n];
       for (int i = 0; i < n; i++) {
-        A[i] = In.readInt(); // element of array
+        A[i] = In.readInt(); // element of Aay
       }
       Out.println(minimumDistance(n, A));
     }
@@ -21,30 +21,50 @@ class Main {
   }
 
   public static int minimumDistance(int n, int[] A) {
-    
-    
+    System.out.println("n: " + n);
+    System.out.println("A.length: " + A.length);
+    int l = 0;
+    int r = n-1;
+    mergeSort(A, l, r);
+    int [] elementCounts = new int[n];
+    int counter = -1;
+    int currentIndex = 0;
+    int absMaxCounter = -1;
+    int tempElement = A[0];
 
-    for (int i = 0; i <= n; ++i) {
-
+    for (int i = 0; i < n; ++i) {
+      if(tempElement == A[i]) {
+        elementCounts[currentIndex]++;
+        counter++;
+        if(counter > absMaxCounter) {
+          absMaxCounter = counter;
+        }
+      } else {
+        tempElement = A[i];
+        counter = 1;
+        currentIndex++;
+      }
     }
-    
-    return 0;
+    return n-absMaxCounter;
   }
 
-  public static int [] mergeSort(int [] A, int l, int r) {
-    // here first sort the Array A
+  public static void mergeSort(int [] A, int l, int r) {
+    // here first sort the Aay A
     if (l < r) {
-      int m = l + r / 2; //check if this works 
+      // THIS DOES NOT WORK: int m = l + r / 2; //check if this works <- I would need a round function from 
+      int m = l + (r - l) / 2;
 
       mergeSort(A, l, m);
       mergeSort(A, m+1, r);
       merge(A, l, m, r);
     }
-
-    return A;
+    //return A;
   }
 
-  public static int [] merge(int [] A, int l, int m, int r) {
+  public static void merge(int [] A, int l, int m, int r) {
+    /*/
+    // Something is off in this merge...
+    // merge of the different subAays of A
     int [] B = new int [r-l+1];
     int i = l;
     int j = m+1;
@@ -61,6 +81,53 @@ class Main {
         k++;
       }
     } 
-    return A;
+    // return A;
   }
+  */
+    // Find sizes of two subAays to be merged
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp Aays
+    int L[] = new int[n1];
+    int R[] = new int[n2];
+
+    // Copy data to temp Aays
+    for (int i = 0; i < n1; ++i)
+        L[i] = A[l + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = A[m + 1 + j];
+
+    // Merge the temp Aays
+
+    // Initial indices of first and second subAays
+    int i = 0, j = 0;
+
+    // Initial index of merged subAay Aay
+    int k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            i++;
+        }
+        else {
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        A[k] = R[j];
+        j++;
+        k++;
+    }
+  }
+  
 }
