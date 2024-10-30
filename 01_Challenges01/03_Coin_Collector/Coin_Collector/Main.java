4ed6331f1463cdaeb6207d074edd52788b9ac777
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-10-30 10:14:36 +0100

    implemented the challenge

--- 01_Challenges01/03_Coin_Collector/Coin_Collector/Main.java
+++ 01_Challenges01/03_Coin_Collector/Coin_Collector/Main.java
@@ -3,9 +3,9 @@
 class Main {
   public static void main(String[] args) {
     // Uncomment the following two lines if you want to read from a file
-    // In.open("public/0-example.in");
-    // Out.compareTo("public/0-example.out");
-
+    // In.open("public/1-small.in");
+    // Out.compareTo("public/1-small.out");
+    /* 
     int testNum = In.readInt(); // number of tests
     for (int test = 0; test < testNum; test++) {
       int n = In.readInt();
@@ -16,14 +16,76 @@ class Main {
       }
       Out.println(coinCollect(n, A, T));
     }
+    */
+
+    int [] A = {3, 1, 4, 2, 5, 3};
+    // int [] A = {3, 1, 4, 2, 10};
+    int T = 8; 
+    int n = A.length;
+    int result = coinCollect(n, A, T);
+    System.out.println(result);
     // Uncomment this line if you want to read from a file
     // In.close();
   }
-    
+  
+  /*
+   if(T == A.length) {
+      int maxElement = -1; 
+      for (int i=0; i <= A.length-1; ++i) {
+        if (maxElement < A[i]) {
+         maxElement = A[i];
+      }
+    }
+      return maxElement;
+    }
+
+    int sum = 0;
+    for(int j=0; j <= A.length-1; ++j) {
+      sum += A[j];
+    }
+    //System.out.println(sum);
+    long fraction = (long) sum/T;
+    //System.out.println(fraction);
+    int X = (int) Math.ceil(fraction); 
+
+    return X;
+   */
 
 
   public static int coinCollect(int n, int[] A, int T) {
-   // TODO:
-   return 0;
+    int maxElement = -1; 
+    for (int i=0; i <= A.length-1; ++i) {
+      if (maxElement < A[i]) {
+        maxElement = A[i];
+      }
+    }
+    // early exit for case where: T=n 
+    if(T == n) {
+      return maxElement;
+    }
+    int leftPointer = 1; 
+    int rightPointer = maxElement; 
+    int X = maxElement;
+
+    while(leftPointer <= rightPointer) {
+      int pivot = leftPointer + (rightPointer - leftPointer) / 2;      
+      int currentTotalSteps = currentTotalSteps(pivot, A);
+
+      if(currentTotalSteps <= T) {
+        X = pivot; 
+        rightPointer = --pivot;
+      } else {
+        leftPointer = ++pivot;
+      }
+    }
+    return X;
+  }
+
+  public static int currentTotalSteps(int currentX, int[] A) {
+    int totalSteps = 0;
+    for(int coin : A) {
+      totalSteps += Math.ceil((double) coin / currentX);
+    }
+    return totalSteps;
   }
 }
