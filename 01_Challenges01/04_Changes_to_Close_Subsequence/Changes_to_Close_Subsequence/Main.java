0b5b86c4e5914e3e77ca3e7a1cabf1f18fda1baa
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-11-03 19:26:58 +0100

    first version => not wokring

--- 01_Challenges01/04_Changes_to_Close_Subsequence/Changes_to_Close_Subsequence/Main.java
+++ 01_Challenges01/04_Changes_to_Close_Subsequence/Changes_to_Close_Subsequence/Main.java
@@ -30,7 +30,39 @@ class Main {
   }
 
   public static int minChanges(int n, int m, int k, int[] A, int[] B) {
-    // TODO
-    return 0;
+    int [][] dpTable = new int [B.length] [A.length]; 
+    
+    for (int i=0; i <= B.length-1; ++i) {
+      for (int j=0; j <= A.length-1; ++j) {
+        // BASE CASE 1: (?)
+        if(i == 0) {
+          dpTable [i][j] = needChanges(A[j], B[i], k);
+        }
+        // BASE CASE 2: (?)
+        if (i > j) {
+          dpTable [i][j] = 2147483646; 
+        }
+        if (i!=0 && j!=0 && dpTable[i-1][j-1] != 2147483646) {
+          dpTable[i][j] = needChanges(n, m, k) + dpTable[i-1][j-1];   
+        }
+        
+        
+      }
+    }
+    return getMinElement(dpTable, B.length-1, A.length-1);
   }
+
+  public static int needChanges(int elementA, int elementB, int k) {
+    return (Math.abs(elementA - elementB) <= k) ? 0 : 1;
+  }
+  
+  public static int getMinElement(int [][] dpTable, int indexLastRow, int yMax) {
+    int minValueLastRow = 2147483646;
+    for (int i=0; i < yMax; ++i) {
+      if(dpTable[indexLastRow][i] < minValueLastRow) {
+        minValueLastRow = dpTable[indexLastRow][i];
+      }
+    }
+    return minValueLastRow;
+  } 
 }
