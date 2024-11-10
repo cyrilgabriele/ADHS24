dd9545417d6ebcf4d6d6534019f7e6245ab8e70b
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-11-10 17:15:41 +0100

    implmentation of the DP problem of the series

---
+++ 00_Algorithms/DP/Serie7/SubsetSumWDuplicates.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+
+public class SubsetSumWDuplicates {
+    
+    public static void main(String[] args) {
+        int[] A = {-1, 3, 4, 2, 7};
+        int n = A.length-1;
+        int b = 22;
+        int [] [] [] DP = new int[3][n+1][b+1];
+       
+        for(int t=0; t <= 2; t++) {
+            for(int i=0; i<=n; i++) {
+                for(int s=0; s<=b; s++) {
+                    DP[t][i][s] = -1;
+                }
+            }
+        }
+
+        System.out.println("Output:  " + subsetSumWDuplicates(n, b, DP, A));
+
+
+    }
+
+    public static boolean subsetSumWDuplicates(int n, int b, int [][][] DP, int[] A) {
+        for(int t=0; t<=2; t++) {
+            for(int i=0; i<=n; i++) {
+                for(int s=0; s<=b; s++) {
+                    // BASE CASE 1:
+                    if(t==0 && s>=1) {
+                        DP[t][i][s] = 0;
+                    }
+                    // BASE CASE 2:
+                    if(i==0 && s>=1) {
+                        DP[t][0][s] = 0;
+                    }
+                    // BASE CASE 3:
+                    if(s==0) {
+                        DP[t][i][s] = 1;
+                    }
+                }
+            }
+        }
+
+        // Recurrence: loops begin w/ 1 bc otherwise in base cases
+        for(int t=2; t <= 2; t++) { // max value of t is given from exercise description
+            for(int i=1; i<=n; i++) {
+                for(int s=1; s<= b; s++) {
+                    DP[t][i][s] = 0;
+                    if(DP[t][i-1][s] == 1) {
+                        DP[t][i][s] = 1;
+                    }
+                    boolean sIsSmaller = s < A[i];
+                    if (!sIsSmaller) { // chech for out of bounds!
+                        if(DP[t][i-1][s-A[i]] == 1) {
+                            DP[t][i][s] = 1;
+                        } 
+                    }
+                    if(s >= 2*A[i]){
+                        if(DP[t][i-1][s-2*A[i]] == 1) DP[t][i][s] = 1;
+                    }
+                }
+            }
+        }
+        return DP[2][n][b] == 1;
+    }
+
+    public static void printDPTable(int[][][] DP, int t) {
+        System.out.println("DP Table for t = " + t + ":");
+        for (int i = 0; i < DP[t].length; i++) {
+            for (int j = 0; j < DP[t][i].length; j++) {
+                // Print each element in the row
+                System.out.print(DP[t][i][j] + " ");
+            }
+            // Move to the next row
+            System.out.println();
+        }
+        System.out.println(); // Extra line for spacing between tables
+    }
+    
+}
--- 01_Challenges01/04_Changes_to_Close_Subsequence/Changes_to_Close_Subsequence/Main.java
+++ 01_Challenges01/04_Changes_to_Close_Subsequence/Changes_to_Close_Subsequence/Main.java
@@ -5,7 +5,7 @@ class Main {
     // Uncomment the following two lines if you want to read from a file
     // In.open("public/0-example.in");
     // Out.compareTo("public/0-example.out");
-
+    /* 
     int testNum = In.readInt(); // number of tests
     for (int test = 0; test < testNum; test++) {
       int n = In.readInt();
@@ -27,31 +27,47 @@ class Main {
 
     // Uncomment this line if you want to read from a file
     // In.close();
+    */
+    int [] A = {3,6,3,3,2,-8};
+    int [] B = {1,7,5,-9};
+    int k = 1;
+
+    int output = minChanges(1, 1, k, A, B);
+    System.out.println(output);
+
   }
 
   public static int minChanges(int n, int m, int k, int[] A, int[] B) {
     int [][] dpTable = new int [B.length] [A.length]; 
     
-    for (int i=0; i <= B.length-1; ++i) {
+    int minValuePreviousRow = 2147483645;
+    for (int i=0; i <= B.length-1; ++i) { 
       for (int j=0; j <= A.length-1; ++j) {
         // BASE CASE 1: (?)
         if(i == 0) {
-          dpTable [i][j] = needChanges(A[j], B[i], k);
+          dpTable [i][j] = needChange(A[j], B[i], k);
         }
         // BASE CASE 2: (?)
         if (i > j) {
           dpTable [i][j] = 2147483646; 
         }
         if (i!=0 && j!=0 && dpTable[i-1][j-1] != 2147483646) {
-          dpTable[i][j] = needChanges(A[j], B[i], k) + dpTable[i-1][j-1];   
+          //dpTable[i][j] = needChange(A[j], B[i], k) + dpTable[i-1][j-1];
+          dpTable[i][j] = needChange(A[j], B[i], k) + minValuePreviousRow;   
+        }
+        if(dpTable[i][j] < minValuePreviousRow) {
+          minValuePreviousRow = dpTable[i][j];
+          System.out.println("minValue: " +minValuePreviousRow);
         }
       }
+      minValuePreviousRow = 0;
     }
+    printMatrix(dpTable, A, B);
     return getMinElement(dpTable, B.length-1, A.length-1);
   }
 
-  public static int needChanges(int elementA, int elementB, int k) {
-    return (Math.abs(elementA - elementB) <= 1) ? 0 : 1;
+  public static int needChange(int elementA, int elementB, int k) {
+    return (Math.abs(elementA - elementB) <= k) ? 0 : 1;
   }
   
   public static int getMinElement(int [][] dpTable, int indexLastRow, int yMax) {
@@ -63,4 +79,12 @@ class Main {
     }
     return minValueLastRow;
   } 
+
+  public static void printMatrix(int [][] dpTable, int [] A, int [] B) {
+    for (int i=0; i <= B.length-1; ++i) {
+      for (int j=0; j <= A.length-1; ++j) {
+        System.out.println(dpTable[i][j]);
+      }
+    }
+  }
 }
