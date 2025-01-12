66f03a036f32269dbe2d9d99acc4f66b3434b5e4
Cyril Gabriele <cyril.gabriele@gmail.com>
2025-01-12 10:28:21 +0100

    added the early exit

--- src/00_Algorithms/Factorial.java
+++ src/00_Algorithms/Factorial.java
@@ -1,12 +1,17 @@
 public class Factorial {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 0;
         int result = factorial(n);
         System.out.println("result: " + result);
         
     }
 
     public static int factorial(int n) {
+        // need to test this otherwise wrong for n=0!
+        if(n==0) {
+            return 1;
+        }
+
         int factorial = n;
 
         for(int i=1; i<=n-1; i++) {
