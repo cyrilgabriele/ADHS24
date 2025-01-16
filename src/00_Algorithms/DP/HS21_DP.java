75e6b126744652985f1a4bce9e26a4353ad118a5
Cyril Gabriele <cyril.gabriele@gmail.com>
2025-01-16 14:31:38 +0100

    minor changes in format

--- src/00_Algorithms/DP/HS21_DP.java
+++ src/00_Algorithms/DP/HS21_DP.java
@@ -10,7 +10,7 @@ public class HS21_DP {
     }
 
     public static int dpAlgo(int n, int A, int B, int[] array) {
-        int[][][] dp = new int[n + 1][A + 1][B + 1];
+        int[][][] dp = new int[n+1][A+1][B+1];
 
         // BASE CASES
         for (int i=0; i <= n; i++) {  // Empty subset is always valid for sum = 0 and sum-of-squares = 0
@@ -18,20 +18,18 @@ public class HS21_DP {
         }
 
         // RECURSION
-        for (int i = 1; i <= n; i++) {          // Iterate over elements
-            for (int j = 0; j <= A; j++) {      // Iterate over all possible sums
-                for (int k = 0; k <= B; k++) {  // Iterate over all possible sum-of-squares
+        for (int i=1; i<=n; i++) {          
+            for (int j=0; j<=A; j++) {      
+                for (int k=0; k<=B; k++) {  
                     // Case 1: Exclude the current element
-                    dp[i][j][k] = dp[i - 1][j][k];
-
+                    dp[i][j][k] = dp[i-1][j][k];
                     // Case 2: Include the current element, if valid
-                    if (j - array[i - 1] >= 0 && k - (array[i - 1] * array[i - 1]) >= 0) {
-                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - array[i - 1]][k - (array[i - 1] * array[i - 1])]);
+                    if (((j - array[i-1]) >= 0) && ((k - (array[i-1] * array[i-1])) >= 0)) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i-1][j - array[i-1]][k - (array[i-1] * array[i-1])]);
                     }
                 }
             }
         }
-
-        return dp[n][A][B]; // Result: Can we form subset with sum A and sum-of-squares B?
+        return dp[n][A][B]; 
     }
 }
